c1d9d3437245d52f570afe6f3a52908fe177dc0e
bbno5 <136372594+bbno5@users.noreply.github.com>
2024-01-06 14:26:24 +0200

    Add files via upload

---
+++ gun.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Gun(Sprite):
+
+    def __init__(self, screen):
+        super(Gun, self).__init__()
+        self.screen = screen
+        self.image = pygame.image.load('images/pixil-frame-1.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.centre = float(self.rect.centerx)
+        self.rect.bottom = self.screen_rect.bottom
+        self.mright = False
+        self.mleft = False
+
+    def output(self):
+        self.screen.blit(self.image, self.rect)
+
+    def update_gun(self):
+        if self.mright and self.rect.right < self.screen_rect.right:
+            self.centre += 0.5
+        if self.mleft and self.rect.left > 0:
+            self.centre -= 0.5
+        self.rect.centerx = self.centre
+
+    def create_gun(self):
+
+        self.center = self.screen_rect.centerx
+
